9d33b09fc202de476734668ce5b024200f23ff3d
98-Fernando <fer91023@gmail.com>
2025-10-16 15:40:18 -0500

    Update Ticket.js

--- backend/models/Ticket.js
+++ backend/models/Ticket.js
@@ -1,22 +1,22 @@
 import mongoose from "mongoose";
 
 const TicketSchema = new mongoose.Schema({
-  reference: { type: String, required: true, unique: true }, // referencia de Wompi
-  nombre: { type: String, required: true },
-  correo: { type: String, required: true },
-  telefono: { type: String, required: true },
-  numeros: { type: [Number], required: true }, // consistente con Pendiente
-  estadoPago: {
-    type: String,
-    enum: ["pendiente", "pagado"],
-    default: "pendiente"
-  },
-  fecha: {
-    type: Date,
-    default: Date.now
-  }
+    reference: { type: String, required: true, unique: true },
+    nombre: { type: String, required: true },
+    correo: { type: String, required: true },
+    telefono: { type: String, required: true },
+    numeros: { type: [String], required: true }, 
+    estadoPago: {
+        type: String,
+        enum: ["pendiente", "pagado"],
+        default: "pendiente"
+    },
+    fecha: {
+        type: Date,
+        default: Date.now
+    }
 }, {
-  timestamps: true
+    timestamps: true
 });
 
 export default mongoose.model("Ticket", TicketSchema);
